428322f01fa0b55984c6848b91c4514f639f2a1e
Joe O <j0liverko@gmail.com>
2019-11-04 12:48:51 -0600

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+//Contexts
+import { ProductContext } from '../src/contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -12,30 +15,23 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
+		<ProductContext.Provider value={{ products, addItem }}>
+		<CartContext.Provider value={{ cart }}>
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<Route exact path="/" component={Products} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<Route path="/cart" component={ShoppingCart}/>
+
 		</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
